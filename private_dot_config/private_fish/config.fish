83ba37f81105ef8783b6326ce651664747a8e122
Bas Bossink <bas.bossink@gmail.com>
2022-03-29 03:14:20 +0200

    Add ssh-agent snippet, and some aliases

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -7,7 +7,18 @@ alias apt='doas /usr/bin/apt'
 alias ll='exa --all --long --icons --classify --group-directories-first --sort name --time-style long-iso'
 alias cm='chezmoi'
 alias g='git'
+alias vi='/home/bas/.local/bin/nvim.appimage'
+alias vim='vi'
+alias nvim='vi'
+alias neovim='vi'
+alias gvim='neovide'
+
 
 zoxide init fish | source 
 starship init fish | source 
 
+if test -z (pgrep ssh-agent | string collect)
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
