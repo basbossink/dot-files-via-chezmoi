ce36afc4bb241d4cc5942bec9f248f310d106b2a
Bas Bossink <bas.bossink@gmail.com>
2022-03-31 12:40:52 +0200

    Add doom to path, add some more aliases

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,18 +1,20 @@
 set -U fish_greeting
 set -U SXHKD_SHELL sh
 set -gx SKIM_DEFAULT_COMMAND "fd --type f"
-set -gx PATH $HOME/.cargo/bin $HOME/.local/bin $PATH
+set -gx PATH ~/.emacs.d/bin $HOME/.cargo/bin $HOME/.local/bin $PATH
+set -gx EDITOR hx
+set -gx VISUAL hx
 
 alias apt='doas /usr/bin/apt'
 alias ll='exa --all --long --icons --classify --group-directories-first --sort name --time-style long-iso'
 alias cm='chezmoi'
 alias g='git'
-alias vi='/home/bas/.local/bin/nvim.appimage'
-alias vim='vi'
-alias nvim='vi'
-alias neovim='vi'
+alias vi='nvim'
+alias vim='nvim'
+alias edit='hx'
+alias e='hx'
+alias neovim='nvim'
 alias gvim='neovide'
-
 
 zoxide init fish | source 
 starship init fish | source 
